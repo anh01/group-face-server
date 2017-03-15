23123598fdbbc0f523bbbc1eed6182d5e59b8a9c
Admin <vanpho02@gmail.com>
2017-03-15 15:49:59 +0700

    get Face from url finish

--- api/getFaceFromImage.js
+++ api/getFaceFromImage.js
@@ -2,7 +2,7 @@ const request = require('then-request');
 
 const KEY = '6849f848e58547a3997fd750e0ac4a3b';
 
-function getFaceIdsFromImageUrl(imageUrl) {
+function getFaceIdsFromImage(imageUrl) {
     const url = 'https://westus.api.cognitive.microsoft.com/face/v1.0/detect';
     return request('POST', url, { 
         headers: { 'Ocp-Apim-Subscription-Key': KEY },
@@ -13,11 +13,12 @@ function getFaceIdsFromImageUrl(imageUrl) {
     .then(res => res.getBody('utf8'))
     .then(result => {
         const arr = JSON.parse(result);
+        if (arr.length === 0) throw new Error('NO_FACE_IN_IMAGE');
         return arr.map(e => e.faceId);
     });
 }
 
-module.exports = getFaceIdsFromImageUrl;
+module.exports = getFaceIdsFromImage;
 
 // uploadFace('https://khoapham-face.herokuapp.com/tien.png')
 // .then(res => console.log(res));
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const formidble = require('express-formidable')({ uploadDir: './public' });
 const fs = require('fs');
 const rename = require('./api/rename');
+const getFaceIds = require('./api/getFaceFromImage');
+
 // const getFaceByImageUrl = require('./api/uploadFace');
 // const getNameByFaceId = require('./api/identify');
 
@@ -11,25 +13,25 @@ app.listen(process.env.PORT || 3000, () => console.log('server started'));
 
 app.get('/', (req, res) => res.send('Still alive'));
 
-// app.post('/findByUrl', parser, (req, res) => {
-    //     const { imageUrl } = req.body;
-    //     console.log('URL:::', imageUrl);
-    //     getFaceByImageUrl(imageUrl)
-    //     .then(faceId => {
-    //         console.log('FACE ID:', faceId);
-    //         return getNameByFaceId(faceId);
-    //     })
-    //     .then(name => { 
-    //         console.log('NAME:', name);
-    //         res.send(`${name} `);
-    //     })
-    //     .catch(err => console.log(err));
-    // });
-//Upload image
 app.post('/upload', formidble, async (req, res) => {
     const { path } = req.files.avatar;
-    rename(path)
-    .then(filename => res.send(filename));
+    try {
+        const filename = await rename(path);
+        const faceIds = await getFaceIds(`https://groupkhoapham.herokuapp.com/${filename}`);
+        res.send(faceIds);
+    } catch (e) {
+        res.send(`${e} `);
+    }
+});
+
+app.get('/test/:filename', (req, res) => {
+    const { filename } = req.params;
+    getFaceIds(`https://groupkhoapham.herokuapp.com/${filename}`)
+    .then(faceIds => {
+        console.log(faceIds);
+        res.send('xong');
+    })
+    .catch(err => console.log(err));
 });
 
 app.get('/list', (req, res) => {
