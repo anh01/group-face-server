d551a0365eeea5a3612e9ebca44ea68441b9f5e6
Admin <vanpho02@gmail.com>
2017-03-15 16:31:29 +0700

    get people from faceIds

--- api/identify.js
+++ api/identify.js
@@ -1,30 +1,28 @@
 const request = require('then-request');
-const arrayEmp = require('../data');
+// const arrayEmp = require('../data');
 
 const groupId = 'employee';
 const KEY = '6849f848e58547a3997fd750e0ac4a3b';
 
-function getText(text, post, pre) {
-  const start = text.indexOf(post) + post.length;
-  const stop = text.indexOf(pre);
-  return text.substring(start, stop);
-}
+// function getText(text, post, pre) {
+//   const start = text.indexOf(post) + post.length;
+//   const stop = text.indexOf(pre);
+//   return text.substring(start, stop);
+// }
 
-function identify(faceId) {
+function identify(faceIds) {
     const url = 'https://westus.api.cognitive.microsoft.com/face/v1.0/identify';
     return request('POST', url, { 
         headers: { 'Ocp-Apim-Subscription-Key': KEY },
         json: {    
             personGroupId: groupId,
-            faceIds: [faceId],
+            faceIds,
             maxNumOfCandidatesReturned: 1,
             confidenceThreshold: 0.5
         }
-     }).then(res => {
-        const data = res.getBody('utf8').replace('\n', '');
-        const id = getText(data, '"personId":"', '","confidence"');
-        return arrayEmp.find(e => e.personId === id).name;
-     });
+     })
+     .then(res => JSON.parse(res.getBody('utf8')))
+     .then(arr => arr.map(e => e.candidates[0].personId));
 }
 
 module.exports = identify;
--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const formidble = require('express-formidable')({ uploadDir: './public' });
 const fs = require('fs');
 const rename = require('./api/rename');
 const getFaceIds = require('./api/getFaceFromImage');
+const jsonParser = require('body-parser').json();
 
 // const getFaceByImageUrl = require('./api/uploadFace');
-// const getNameByFaceId = require('./api/identify');
+const getResponseFromFaceIds = require('./api/identify');
 
 const app = express();
 app.use(express.static('public'));
@@ -25,9 +26,24 @@ app.post('/upload', formidble, async (req, res) => {
     }
 });
 
+app.post('/findListFace', jsonParser, (req, res) => {
+    const faceIds = [
+        '3134911c-05a2-4e2f-a38a-94de533a2666',
+		'91581496-6148-4bc7-aa12-292a1acf3ca9'
+    ];
+    getResponseFromFaceIds(faceIds)
+    .then(response => {
+        console.log(response);
+        const output = JSON.parse(response);
+        console.log(output[0].candidates);
+        res.send('Hello');
+    });
+});
+
 app.get('/list', (req, res) => {
   fs.readdir('./public', (err, files) => {
     if (err) return res.send(`${err} `); 
     res.send(files.join('\n'));
   });
 });
+
